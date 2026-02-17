48bd5e5ebcadec34177d1e932edeb71ce1588617
CosmicOli <cosmicoli13@gmail.com>
2026-02-17 05:08:58 +0000

    Basic item replacing testing code

--- Open-Ended-Item-Replacer/Open-Ended-Item-Replacer/Open_Ended_Item_Replacer.cs
+++ Open-Ended-Item-Replacer/Open-Ended-Item-Replacer/Open_Ended_Item_Replacer.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using GlobalSettings;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,43 @@ using UnityEngine.SceneManagement;
 
 namespace Open_Ended_Item_Replacer
 {
+    public class GenericSavedItem : SavedItem
+    {
+        public override void Get(bool showPopup = true)
+        {
+            ManualLogSource logSource = new ManualLogSource("logSource");
+
+            // Show popup (if showPopup)
+            // Send get request
+            logSource.LogMessage("Item get");
+        }
+
+        public override bool CanGetMore()
+        {
+            return true;
+        }
+
+        public override Sprite GetPopupIcon()
+        {
+            if (Application.isPlaying)
+            {
+                Debug.LogException(new NotImplementedException());
+            }
+
+            return null;
+        }
+
+        public override string GetPopupName()
+        {
+            if (Application.isPlaying)
+            {
+                Debug.LogException(new NotImplementedException());
+            }
+
+            return null;
+        }
+    }
+
     [BepInPlugin("com.oli.OEIR", "OEIR", "1.0.0")]
     public class Open_Ended_Item_Replacer : BaseUnityPlugin
     {
@@ -72,34 +110,89 @@ namespace Open_Ended_Item_Replacer
 
             foreach (GameObject gameObject in gameObjectsInCurrentScene)
             {
-                Console.WriteLine(gameObject.transform.name);
+                //Console.WriteLine(gameObject.transform.name);
             }
         }
 
+        // Seems to have issues replacing more than one item in a room
+        static bool replacing = true;
         [HarmonyPostfix]
         [HarmonyPatch(typeof(CollectableItemPickup), "OnEnable")]
-        private static void OnEnablePostfix()
+        private static void OnEnablePostfix(CollectableItemPickup __instance)
         {
-            logSource.LogMessage("Collectable enabled");
-        }
-
-        /*[HarmonyPostfix]
-        [HarmonyPatch(typeof(NailSlash), "StartSlash")]
-        public static void StartSlashPostfix(NailSlash __instance)
-        {
-            logSource.LogMessage("Slash Postfix");
-
             GameManager GameManager = GameManager.instance;
             HeroController heroControl = GameManager.hero_ctrl;
 
-            Scene CurrentScene = SceneManager.GetActiveScene();
+            logSource.LogMessage("Pickup Enabled");
+            logSource.LogInfo("Pickup At: " + __instance.transform.position);
 
-            GameObject[] rootObjects = CurrentScene.GetRootGameObjects();
-
-            foreach (GameObject rootObject in rootObjects)
+            if (replacing)
             {
-                Console.WriteLine(rootObject.transform.name);
+                replacing = false;
+                Destroy(__instance.gameObject);
+                logSource.LogInfo("Deleted Pickup Containing: " + __instance.Item.name);
+
+                GenericSavedItem testItem = ScriptableObject.CreateInstance<GenericSavedItem>();
+                testItem.name = "testItem";
+
+                SpawnItemDrop(testItem, 1, null, __instance.gameObject.transform, 1, new Vector3(0, 0, 0));
+                logSource.LogInfo("Item Drop Attempted");
             }
-        }*/
+            else
+            {
+                replacing = true;
+            }
+        }
+
+        private static void SpawnItemDrop(SavedItem dropItem, int count, CollectableItemPickup prefab, Transform spawnPoint, int limit, Vector3 effectOrigin)
+        {
+            if (!dropItem || !dropItem.CanGetMore())
+            {
+                return;
+            }
+
+            if (!prefab)
+            {
+                prefab = Gameplay.CollectableItemPickupPrefab;
+            }
+
+            if (!prefab)
+            {
+                return;
+            }
+
+            Vector3 vector = spawnPoint.TransformPoint(effectOrigin);
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 position = vector;
+                PrefabCollectable prefabCollectable = dropItem as PrefabCollectable;
+                GameObject gameObject;
+                bool flag;
+                if (prefabCollectable != null)
+                {
+                    gameObject = prefabCollectable.Spawn();
+                    flag = false;
+                }
+                else
+                {
+                    CollectableItemPickup collectableItemPickup;
+                    if (limit > 0)
+                    {
+                        collectableItemPickup = ObjectPool.Spawn(prefab.gameObject, null, position, Quaternion.identity, stealActiveSpawned: true).GetComponent<CollectableItemPickup>();
+                    }
+                    else
+                    {
+                        collectableItemPickup = UnityEngine.Object.Instantiate(prefab);
+                        collectableItemPickup.transform.position = position;
+                    }
+
+                    flag = true;
+                    collectableItemPickup.SetItem(dropItem);
+                    logSource.LogInfo("Pickup Item Set: " + dropItem.name);
+                    gameObject = collectableItemPickup.gameObject;
+                    //logSource.LogInfo(collectableItemPickup.Item.name);
+                }
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Silksong-Open-Ended-Item-Replacer
-A mod designed to replace most 'checks' with an an open ended request for an item that other mods can respond to.
+A mod designed to replace most 'checks' with an an open ended request for an item that other mods can respond to.  
+
+This mod is not intended as a great or fully formalised item replacer; it is being made as the more generally accepted and definitely more thought out ItemChanger.Core has not been fully ported to Silksong yet, and I felt it would be fun to try code something together in my free time with a one week deadline.  
+
+Hence though, while I will put some effort into compatibility and reasonable design practices, this project will not bloom into anything comparible to the much more standardised product that the ItemChanger port will soon become.  
+
+Upon functionality being achieved, I likely will not continue to update this, however feel free to report bugs or make pull requests and I'll try have a look.
