e53ce28ab6aaf87b2758fd67e0d5b11ce4c5f1f2
CosmicOli <cosmicoli13@gmail.com>
2026-04-11 02:22:42 +0100

    Added the beginings of main thread blocking async asset grabbing from scenes

---
+++ Open-Ended-Item-Replacer/Open-Ended-Item-Replacer/Silksong/Patches/GameManager Patches/ContinueGame.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System.Collections;
+using System.Threading.Tasks;
+using static Open_Ended_Item_Replacer.Open_Ended_Item_Replacer;
+using static Open_Ended_Item_Replacer.Silksong.Utils.LoadSaveFileUtils;
+
+namespace Open_Ended_Item_Replacer.Silksong.Patches.GameManager_Patches
+{
+    [HarmonyPatch(typeof(GameManager), "ContinueGame")]
+    internal class ContinueGame
+    {
+        public static bool RunContinueGamePatched = true;
+
+        public static bool Prefix()
+        {
+            if (RunContinueGamePatched)
+            {
+                // Starts the async function without awaiting as you cannot await a prefix to halt the continuation of the function from the looks of it
+                DoLoadSaveFileExtras();
+
+                return false;
+            }
+            else
+            {
+                RunContinueGamePatched = true;
+                return true;
+            }
+        }
+    }
+}
--- Open-Ended-Item-Replacer/Open-Ended-Item-Replacer/Silksong/Utils/LoadSaveFileUtils.cs
+++ Open-Ended-Item-Replacer/Open-Ended-Item-Replacer/Silksong/Utils/LoadSaveFileUtils.cs
@@ -1,76 +1,65 @@
-﻿using HutongGames.PlayMaker.Actions;
+﻿using HarmonyLib;
+using HutongGames.PlayMaker.Actions;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 using static Open_Ended_Item_Replacer.Open_Ended_Item_Replacer;
+using static Open_Ended_Item_Replacer.Silksong.Patches.GameManager_Patches.ContinueGame;
+
 
 namespace Open_Ended_Item_Replacer.Silksong.Utils
 {
     internal class LoadSaveFileUtils : MonoBehaviour
     {
-        private static LoadSaveFileUtils dummyMonoBehaviour;
-        private static IEnumerator coroutineIEnumerator;
-
         public static async Task DoLoadSaveFileExtras()
         {
+            logSource.LogWarning("STARTED");
+
             IEnumerable<AssetBundle> preLoadedBundles = AssetBundle.GetAllLoadedAssetBundles();
 
             HeartPieceInstant = preLoadedBundles.Where(x => x.Contains("Assets/Prefabs/Items/Heart Piece Instant.prefab")).First().LoadAsset<GameObject>("Assets/Prefabs/Items/Heart Piece Instant.prefab");
 
-
-            GameObject dummyGameObject = new GameObject();
-            dummyMonoBehaviour = dummyGameObject.AddComponent<LoadSaveFileUtils>();
-
-            //coroutineIEnumerator = LoadScene("Bone_East_05");
-            //dummyMonoBehaviour.StartCoroutine(coroutineIEnumerator);
-
             await LoadScene("Bone_East_05");
 
-            /*PlayerData playerData = PlayerData.instance;
-            string savedRespawnScene;
-            if (!string.IsNullOrEmpty(playerData.tempRespawnScene))
-            {
-                savedRespawnScene = playerData.tempRespawnScene;
-            }
-            else
-            {
-                savedRespawnScene = playerData.respawnScene;
-            }
-            logSource.LogMessage(savedRespawnScene);
-            coroutine = LoadScene(savedRespawnScene);
-            dummyMonoBehaviour.StartCoroutine(coroutine);*/
+            logSource.LogWarning("FINISHED");
 
-            //logSource.LogMessage(coroutine.Current);
-
-            /*AssetBundle Bone_East_05 = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "aa/StandaloneWindows64/scenes_scenes_scenes/Bone_East_05.bundle"));
-            Scene scene = Bone_East_05.LoadAssetWithSubAssets(Bone_East_05.GetAllScenePaths()[0]);
-            
-            logSource.LogInfo(scene.name);
-            logSource.LogInfo(scene.GetRootGameObjects().Length);
-            logSource.LogInfo(scene.isLoaded);
-            foreach (GameObject rootGameObject in scene.GetRootGameObjects())
-            {
-                // pick barrel flea, see if I can unload the scene and keep the object
-                logSource.LogWarning(rootGameObject.name);
-            }
-            //SceneManager.LoadScene(Bone_East_05.);
-            Bone_East_05.Unload(false);*/
+            RunContinueGamePatched = false;
+            GameManager gameManager = GameManager.instance;
+            gameManager.StartCoroutine(gameManager.RunContinueGame(gameManager.IsMenuScene()));
         }
+
+        // Issues: Need to pause the game loading halfway through the continue game function to allow for the correct scene to load first to establish hero controller and also for the loading screen to show
 
         public static async Task LoadScene(string sceneName)
         {
             // Ensure all of these are successfully loaded by other async/co before continueing
-            while (HeroController.instance == null || GameManager.instance == null || GameCameras.instance == null || PlayerData.instance == null)
+            //while (HeroController.instance == null || GameManager.instance == null || GameCameras.instance == null || PlayerData.instance == null)
             {
-                await Task.Delay(100);
+                //Traverse.Create<HeroController>().Field("_instance").SetValue(new HeroController());
+                //await Task.Delay(100);
             }
 
+            AsyncOperationHandle<GameObject> handle2 = GameManager.instance.LoadGlobalPoolPrefab();
+            await handle2.Task;
+            Instantiate(handle2.Result);
+            ObjectPool.CreateStartupPools();
+            handle2 = GameManager.instance.LoadHeroPrefab();
+            await handle2.Task;
+            logSource.LogWarning("MOVED");
+            Instantiate(handle2.Result);
+
+            //await Task.Delay(5000);
+
             // Should do a check first whether the asset bundle has already been loaded, in the case where you either spawn in a given room or a different mod keeps the scene loaded
-            AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "aa/StandaloneWindows64/scenes_scenes_scenes/" + sceneName + ".bundle"));
+            AssetBundleCreateRequest assetBundleCreateRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, "aa/StandaloneWindows64/scenes_scenes_scenes/" + sceneName + ".bundle"));
+
+            await assetBundleCreateRequest;
+            AssetBundle assetBundle = assetBundleCreateRequest.assetBundle;
 
             LoadSceneParameters loadSceneParameters = new LoadSceneParameters(LoadSceneMode.Additive);
             string path = assetBundle.GetAllScenePaths()[0];
@@ -79,14 +68,26 @@ namespace Open_Ended_Item_Replacer.Silksong.Utils
             await sceneLoad;
 
             Scene scene = SceneManager.GetSceneByName(sceneName);
-            logSource.LogInfo(scene.name);
-            logSource.LogInfo(scene.GetRootGameObjects().Length);
-            logSource.LogInfo(scene.isLoaded);
+
+
+            /*(SetupSceneRefs(refreshTilemapInfo: false);
+            yield return null;
+            yield return null;
+            Platform.Current.SetSceneLoadState(isInProgress: false);
+            needFirstFadeIn = true;
+            isLoading = false;
+            if (hero_ctrl == null)
+            {
+                SetupHeroRefs();
+            }*/
+
             foreach (GameObject rootGameObject in scene.GetRootGameObjects())
             {
                 // pick barrel flea, see if I can unload the scene and keep the object
                 //logSource.LogWarning(rootGameObject.name);
             }
+
+            await Task.Delay(1000);
 
             AsyncOperation sceneUnload = SceneManager.UnloadSceneAsync(scene);
             await sceneUnload;
@@ -94,43 +95,5 @@ namespace Open_Ended_Item_Replacer.Silksong.Utils
             AsyncOperation assetBundleUnload = assetBundle.UnloadAsync(false);
             await assetBundleUnload;
         }
-
-        /*public static IEnumerator LoadScene(string sceneName)
-        {
-            while (HeroController.instance == null)
-            {
-                yield return null;
-            }
-
-            // Should do a check first whether the asset bundle has already been loaded, in the case where you either spawn in a given room or a different mod keeps the scene loaded
-            AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "aa/StandaloneWindows64/scenes_scenes_scenes/" + sceneName + ".bundle"));
-
-            LoadSceneParameters loadSceneParameters = new LoadSceneParameters(LoadSceneMode.Additive);
-            string path = assetBundle.GetAllScenePaths()[0];
-            AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(path, loadSceneParameters);
-
-            while (!sceneLoad.isDone)
-            {
-                yield return null;
-            }
-
-            Scene scene = SceneManager.GetSceneByName(sceneName);
-            logSource.LogInfo(scene.name);
-            logSource.LogInfo(scene.GetRootGameObjects().Length);
-            logSource.LogInfo(scene.isLoaded);
-            foreach (GameObject rootGameObject in scene.GetRootGameObjects())
-            {
-                // pick barrel flea, see if I can unload the scene and keep the object
-                //logSource.LogWarning(rootGameObject.name);
-            }
-
-            AsyncOperation sceneUnload = SceneManager.UnloadSceneAsync(scene);
-            while (!sceneUnload.isDone)
-            {
-                yield return null;
-            }
-
-            assetBundle.Unload(false);
-        }*/
     }
 }
